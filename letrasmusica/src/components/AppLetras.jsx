b04ef2cb166a6f7a4b4dcdd1c0400597589ac230
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-04-25 16:28:37 -0500

    Comenzando el proyecto

---
+++ letrasmusica/src/components/AppLetras.jsx
@@ -0,0 +1,17 @@
+import Formulario from "./Formulario";
+
+const AppLetras = () => {
+    return ( 
+        <>
+            <header>Buscador de letras de Canciones</header>
+
+            <Formulario/>
+
+            <main>
+
+            </main>
+        </>
+    );
+}
+ 
+export default AppLetras;
