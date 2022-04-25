b43cb457fc49159039fd65c01576644310ebd0b6
Joel oficina Cues <libra-marriaga@hotmail.com>
2022-04-25 16:39:58 -0500

    añadiendo el formulario

--- letrasmusica/src/components/Formulario.jsx
+++ letrasmusica/src/components/Formulario.jsx
@@ -1,6 +1,46 @@
+import { useState } from 'react'
+
 const Formulario = () => {
+
+    const [busquedaLetra, setBusquedaLetra] = useState({
+        artista: '',
+        cancion: ''
+    })
+
     return ( 
-        <div>Formulario</div>
+        <form>
+            <legend>Busca por Artista y Cancion</legend>
+
+            <div className="form-grid">
+                <div>
+                    <label>Artista</label>
+                    <input 
+                        type="text" 
+                        name="artista" placeholder="Nombre Artista"
+                        value={busquedaLetra.artista}
+                        onChange={e => setBusquedaLetra({
+                            ...busquedaLetra,
+                            [e.target.name]: e.target.value
+                        })}
+                    />
+                </div>
+                <div>
+                    <label>Canción</label>
+                    <input 
+                        type="text" 
+                        name="cancion" placeholder="Nombre Cancion"
+                        value={busquedaLetra.cancion}
+                        onChange={e => setBusquedaLetra({
+                            ...busquedaLetra,
+                            [e.target.name]: e.target.value
+                        })}
+                    />
+                </div>
+                    <input 
+                        type="submit" value="Buscar"
+                    />
+            </div>
+        </form>
     );
 }
  
